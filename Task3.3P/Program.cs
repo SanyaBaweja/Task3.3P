3afb7d9213f44d97872919f01a19e8572c421944
SanyaBaweja <bawejasanya2602@gmail.com>
2023-11-25 15:10:57 +1100

    Initial commit

---
+++ Task3.3P/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using SplashKitSDK;
+namespace ShapeDrawer
+{
+    public class Program
+    {
+
+        public static void Main()
+        {
+            Window window = new Window("Shape Drawer", 800, 600);
+
+            Drawing dr = new Drawing();
+            do
+            {
+                SplashKit.ProcessEvents();
+                SplashKit.ClearScreen();
+                
+
+                
+                if (SplashKit.MouseClicked(MouseButton.LeftButton))
+                {
+                    Shape sh = new Shape();
+
+                    sh.X = SplashKit.MouseX();
+                    sh.Y = SplashKit.MouseY();
+                    dr.AddShape(sh);
+
+                }
+                
+
+                if (SplashKit.MouseClicked(MouseButton.RightButton))
+                {
+                    
+                    Point2D mp = SplashKit.MousePosition();
+                    dr.SelectShapesAt(mp);
+                }
+
+                if (SplashKit.KeyTyped(KeyCode.SpaceKey))
+                {
+
+                    dr.Background1 = SplashKit.RandomRGBColor(255);
+                }
+
+                if (SplashKit.KeyTyped(KeyCode.DeleteKey))
+                {
+                    foreach (Shape s in dr.Selectedshapes)
+                    {
+                        dr.DeleteShape(s);
+                    }
+                }
+
+                if (SplashKit.KeyTyped(KeyCode.BackspaceKey))
+                {
+                    foreach (Shape s in dr.Selectedshapes)
+                    {
+                        dr.DeleteShape(s);
+                    }
+                }
+
+                
+                dr.Draw();
+                SplashKit.RefreshScreen();
+            } while (!window.CloseRequested);
+        }
+    }
+
+}
+
+
+
+
+
---
+++ Task3.3P/Shape.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using ShapeDrawer;
+using SplashKitSDK;
+
+namespace ShapeDrawer
+{
+    public class Shape
+    {
+        private Color _color;
+        private float _x;
+        private float _y;
+        private int _width;
+        private int _height;
+        private bool _selected;
+
+        public Shape()
+        {
+            _color = Color.Blue;
+            _x = (float)0;
+            _y = (float)0;
+            _width = (int)100;
+            _height = (int)100;
+            _selected = (bool)false;
+        }
+        public void Draw()
+        {
+            if (Selected == true)
+            {
+                Outline();
+            }
+            SplashKit.FillRectangle(_color, _x, _y, _width, _height);
+
+        }
+        public Color Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+            }
+        }
+
+        public float X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+        public float Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+
+        }
+        public int Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                _width = value;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                _height = value;
+            }
+
+        }
+        public bool Selected
+        {
+            get
+            {
+                return _selected;
+            }
+            set
+            {
+                _selected = value;
+            }
+        }
+        public bool IsAt(Point2D pt)
+        {
+            if (pt.X >= _x && pt.X < (_x + _width) && pt.Y >= _y && pt.Y <= (_y + _height))
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Outline()
+        {
+            SplashKit.FillRectangle(Color.Black, _x - 2, _y - 2, _width + 4, _height + 4);
+        }
+
+    }
+}
+
+
+
